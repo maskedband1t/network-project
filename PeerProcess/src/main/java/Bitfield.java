e6e50f2f012e57c5d6471d58fc2d13d62acad52d
briankoehler <briankoehler@ufl.edu>
2021-10-20 00:26:21 -0400

    bitfield implementation using bitset

---
+++ PeerProcess/src/main/java/Bitfield.java
@@ -0,0 +1,56 @@
+import java.util.BitSet;
+
+public class Bitfield {
+    private BitSet bits;
+
+    // if you want to calculate the piece count yourself
+    public Bitfield(int pieceCount) {
+        bits = new BitSet(pieceCount);
+    }
+
+    // if you want to just throw the file size and piece size in
+    public Bitfield(long fileSize, long pieceSize) {
+        bits = new BitSet(Math.toIntExact(fileSize / pieceSize + (fileSize % pieceSize == 0 ? 0 : 1)));
+    }
+    
+    public byte[] toByteArray() {
+        return bits.toByteArray();
+    }
+
+    public String toString() {
+        return bits.toString();
+    }
+
+    public BitSet getBits() {
+        return bits;
+    }
+    
+    public int getSize() {
+        return bits.size();
+    }
+
+    // return true if has piece (i.e. bit at index is 1)
+    public boolean hasPiece(int pieceIndex) {
+        try {
+            return bits.get(pieceIndex);
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.err.println("Unable to get this piece: index out of bounds.");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // return true if piece is successfully set
+    public boolean setPiece(int pieceIndex) {
+        try {
+            bits.set(pieceIndex);
+            return true;
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.err.println("Unable to set this piece: index out of bounds.");
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
