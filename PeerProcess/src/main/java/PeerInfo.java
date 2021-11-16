7bea39b8d87a1ae6c791b0c6149eff955adeb116
Anurag Akkiraju <anurag.akkiraju@gmail.com>
2021-11-16 17:35:33 -0500

    added Optimistic Unchoker class

--- PeerProcess/src/main/java/PeerInfo.java
+++ PeerProcess/src/main/java/PeerInfo.java
@@ -1,18 +1,38 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 public class PeerInfo {
     private int peerId;
     private String hostname;
     private int port;
+    private final AtomicBoolean _interested;
+    public AtomicInteger _downloadRate; // want public accessibility here. NOTE: how much did it download over last interval
+     // should we add bitset here?
 
     public PeerInfo() {
         peerId = -1;
         hostname = "";
         port = -1;
+        _interested = new AtomicBoolean (false);
+        _downloadRate = new AtomicInteger(0);
     }
 
     public PeerInfo(int peerId, String hostname, int port) {
         this.peerId = peerId;
         this.hostname = hostname;
         this.port = port;
+        _interested = new AtomicBoolean (false);
+        _downloadRate = new AtomicInteger(0);
+    }
+
+    public boolean isInterested() {
+        return _interested.get();
+    }
+    public void setInterested() {
+        _interested.set (true);
+    }
+
+    public void setNotIterested() {
+        _interested.set (false);
     }
 
     public int getId() {
--- PeerProcess/src/main/java/PeerManager.java
+++ PeerProcess/src/main/java/PeerManager.java
@@ -1,7 +1,52 @@
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.Collections;
 import java.util.List;
 
-public class PeerManager {
+public class PeerManager implements Runnable {
+
+    class OptimisticUnchoker extends Thread {
+        private final int _num_optimistic_unchoked_neighbors;
+        private final int _optimistic_unchoking_interval;
+        private final List<PeerInfo> _chokedPeers = new ArrayList<>();
+        // TODO: set up collection of optimistically unchoked peers
+        final Collection<PeerInfo> _optimisticallyUnchokedPeers = Collection.newSetFromMap(); // not done
+        
+        OptimisticUnchoker(){
+            _num_optimistic_unchoked_neighbors = 1; // hardcoded for now (maybe don't need to change)
+            _optimistic_unchoking_interval = CommonConfig.getInstance().optimisticUnchokingInterval;
+        }
+
+        synchronized void setChokedPeers(Collection<PeerInfo> chokedPeers) { // shouldn't be able to access this while running
+            _chokedPeers.clear();
+            _chokedPeers.addAll(chokedPeers);
+        }
+
+        @Override
+        public void run(){
+            while(true){
+                try {
+                    Thread.sleep(_optimistic_unchoking_interval); // constantly sleeping for interval and reshuffling once out
+                } catch (InterruptedException e) {
+                    //TODO: handle exception
+                }
+
+                synchronized(this){
+                        if(!_chokedPeers.isEmpty()){
+                            Collections.shuffle(_chokedPeers); // shuffle to pick new unchoked peers 
+                            _optimisticallyUnchokedPeers.clear(); // clear to put new ones in
+                            _minPeers = Math.min(_chokedPeers.size() ,_num_optimistic_unchoked_neighbors);
+                            _optimisticallyUnchokedPeers.addAll(0, _minPeers); // since already shuffled, this is fine
+                        }
+                }
+            }
+        }
+    }
+
+
+
+
+
     public List<Integer> _interestedPeers;
     public List<Integer> _preferredPeers;
     public List<Integer> _optimisticallyUnchokedPeers;
